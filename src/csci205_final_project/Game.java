70509c9af96e17fa1343ed1c8ce31fd2208694d4
Eben Blaisdell <emb038@bucknell.edu>
2016-11-08 15:24:53 -0500

    MVC more basic setup

--- src/csci205_final_project/Game.java
+++ src/csci205_final_project/Game.java
@@ -5,10 +5,44 @@
  */
 package csci205_final_project;
 
+import csci205_final_project.ctrl.Ctrl;
+import csci205_final_project.model.Model;
+import csci205_final_project.view.View;
+import java.io.IOException;
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
 /**
  *
  * @author emb038
  */
-public class Game {
+public class Game extends Application {
+    View theView;
+    Model theModel;
+    Ctrl theCtrl;
+
+    public static void main(String[] args) throws IOException {
+	launch(args);
+    }
+
+    @Override
+    public void init() throws IOException {
+	theModel = new Model();
+	theView = new View(theModel);
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+	theCtrl = new Ctrl(theModel, theView);
+
+	// Now, set up the scene, and connect it to the stage!
+	Scene scene = new Scene(theView.getRoot());
+
+	primaryStage.setTitle("ZTD 47");
+	primaryStage.setScene(scene);
+	primaryStage.sizeToScene();
+	primaryStage.show();
+    }
 
 }
