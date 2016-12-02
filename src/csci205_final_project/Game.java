c299111264805a81856025148d7f800b63d8f5d1
Xeniya Tsoktoyeva <xt004@bucknell.edu>
2016-12-02 14:06:38 -0500

    screens

--- src/csci205_final_project/Game.java
+++ src/csci205_final_project/Game.java
@@ -1,16 +1,23 @@
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
+
 package csci205_final_project;
 
 import csci205_final_project.ctrl.Ctrl;
 import csci205_final_project.model.Model;
 import csci205_final_project.view.View;
+import csci205_final_project.view.ViewGameOver;
+import csci205_final_project.view.ViewStart;
 import java.io.IOException;
 import javafx.application.Application;
+import javafx.event.EventHandler;
 import javafx.scene.Scene;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
 
 /**
@@ -21,6 +28,9 @@ public class Game extends Application {
     public static View theView;
     public static Model theModel;
     public static Ctrl theCtrl;
+    public static Stage primaryStage;
+    public static ViewStart theViewStart;
+    public static ViewGameOver theViewGameOver;
 
     public static void main(String[] args) throws IOException {
 	launch(args);
@@ -31,22 +41,61 @@ public class Game extends Application {
     public void init() throws IOException {
 	theModel = new Model();
 	theView = new View(theModel);
+        theViewStart = new ViewStart();
+        theViewGameOver = new ViewGameOver();
 
     }
 
     @Override
     public void start(Stage primaryStage) throws Exception {
+        
+        Scene sceneStart = new Scene(theViewStart.getRoot());
+        this.primaryStage = primaryStage;
+        
+        primaryStage.setTitle("ZTD 47");
+        primaryStage.setScene(sceneStart);
+        primaryStage.sizeToScene();
+        primaryStage.show();
+        
+        setHandler();
+        
+        
 	// Now, set up the scene, and connect it to the stage!
-	Scene scene = new Scene(theView.getRoot());
-	theView.setScene(scene);
+    }
+    
+    public void setHandler() {
+	theViewStart.getRoot().lookup("#start").addEventHandler(
+		MouseEvent.MOUSE_CLICKED,
+		new EventHandler<MouseEvent>() {
+	    @Override
+	    public void handle(MouseEvent event) {
+		try {
+		    	Scene scene = new Scene(theView.getRoot());
+                        theView.setScene(scene);
 
-	theCtrl = new Ctrl(theModel, theView);
-	theModel.addPlayer();
+                        theCtrl = new Ctrl(theModel, theView);
+                        theModel.addPlayer();
 
-	primaryStage.setTitle("ZTD 47");
-	primaryStage.setScene(scene);
-	primaryStage.sizeToScene();
-	primaryStage.show();
+                        primaryStage.setTitle("ZTD 47");
+                        primaryStage.setScene(scene);
+                        primaryStage.sizeToScene();
+                        primaryStage.show();
+                        theView.initTowerButtons();
+                        theView.setViewBindings();
+		} catch (Exception ex) {
+
+		}
+	    }
+	});
+    }
+    
+    public static void gameOver() {
+        Scene sceneStart = new Scene(theViewGameOver.getRoot());
+        primaryStage.setTitle("ZTD 47");
+        primaryStage.setScene(sceneStart);
+        primaryStage.sizeToScene();
+        primaryStage.show();
+        System.out.println("Game Over");
     }
 
 }
