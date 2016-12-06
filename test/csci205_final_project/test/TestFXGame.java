8fbc63c37e9e82ffce46485e1a0ab6316e854a03
Jack Hall-Tipping <jmht001@bucknell.edu>
2016-12-05 21:15:32 -0500

    test

---
+++ test/csci205_final_project/test/TestFXGame.java
@@ -0,0 +1,26 @@
+/* *****************************************
+* CSCI205 - Software Engineering and Design
+* Fall 2016
+*
+* Name: NAMES of team members
+* Date: Dec 5, 2016
+* Time: 5:41:16 PM
+*
+* Project: csci205_final_project
+* Package: csci205_final_project.test
+* File: TestFXGame
+* Description:
+*
+* ****************************************
+ */
+package csci205_final_project.test;
+
+/**
+ *
+ * @author Jack
+ */
+public class TestFXGame extends GuiTest {
+    public Parent getRootNode() {
+        //TODO
+    }
+}
